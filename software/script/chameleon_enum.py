4063c4bbe025f16ca3c23dbdb2efe21d9370d9c3
WillyJL <me@willyjl.dev>
2025-08-23 02:47:04 +0200

    Symmetric animation mode for boot, shutdown, usb

--- software/script/chameleon_enum.py
+++ software/script/chameleon_enum.py
@@ -497,6 +497,7 @@ class MifareClassicDarksideStatus(enum.IntEnum):
 class AnimationMode(enum.IntEnum):
     FULL = 0
     MINIMAL = 1
+    SYMMETRIC = 3
     NONE = 2
 
     def __str__(self):
@@ -504,6 +505,8 @@ class AnimationMode(enum.IntEnum):
             return "Full animation"
         elif self == AnimationMode.MINIMAL:
             return "Minimal animation"
+        elif self == AnimationMode.SYMMETRIC:
+            return "Symmetric animation"
         elif self == AnimationMode.NONE:
             return "No animation"
 
